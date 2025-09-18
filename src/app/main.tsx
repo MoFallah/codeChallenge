e647e06fe233eb2a50b4c6187e4f7d593b437fb1
Mohammad Tafti <hossein.fallah@gmail.com>
2025-09-18 16:54:07 -0400

    fix index and tsconfig

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,7 @@
-// src/main.tsx or index.tsx
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
-import '../index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
