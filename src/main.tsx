ccc1bf2d9a543220b11b31c9e1a8476ebf085b27
Mohammad Tafti <hossein.fallah@gmail.com>
2025-09-18 19:12:24 -0400

    fix styling issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './store';
+import App from './app/App';
+import { store } from './app/store';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
